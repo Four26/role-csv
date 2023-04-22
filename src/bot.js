753301231f30d4ae8b15c09893ffa2e696609d7c
Franklin <fabonero@ssct.edu.ph>
2023-04-22 11:52:59 +0800

    edited the bot

--- src/bot.js
+++ src/bot.js
@@ -1,42 +1,3 @@
-// require('dotenv').config();
-
-// const discord = require('discord.js');
-// const config = require('../config.json');
-// const client = new discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'] });
-// const commandName = config.commandName;
-// const { generateCSV } = require('./functions/csvGenerator');
-
-// client.on('error', (error) => {
-//     consoleLog(error.message, 'ERROR');
-// });
-
-// client.on('ready', () => {
-//     console.log('Ready and connected to Discord.')
-// });
-
-// client.on('messageCreate', async (message) => {
-//     if (!message.content.startsWith(config.commandPrefix) || message.author.bot) {
-//         return; //Ignore messages that don't start with the command prefix or are sent by bots.
-//     };
-
-//     const [command, ...commandArguments] = message.content.slice(config.commandPrefix.length).trim().split(/ +/g).map(arg => arg.toLowerCase());
-
-//     switch (command) {
-//         case `${commandName}`:
-//             if (!message.member.roles.cache.some(role => config.allowedRoles.includes(role.name))) {
-//                 return; //Ignore commands sent by users without allowed roles.
-//             }
-//             await generateCSV(message);
-//             console.log(`Generating CSV file for ${message.guild.name} server. "${message.member.user.username} requested this file."`);
-//             message.delete({ timeout: 250 });
-//             break;
-//         default:
-//             return; //Ignore any other commands.
-//     }
-// });
-
-// client.login(process.env.BOT_TOKEN);
-
 require('dotenv').config();
 
 const discord = require('discord.js');
@@ -78,3 +39,43 @@ client.on('messageCreate', async (message) => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
+// require('dotenv').config();
+
+// const discord = require('discord.js');
+// const config = require('../config.json');
+// const client = new discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'] });
+// const commandName = config.commandName;
+// const { generateCSV } = require('./functions/csvGenerator');
+
+// client.on('error', (error) => {
+//     consoleLog(error.message, 'ERROR');
+// });
+
+// client.on('ready', () => {
+//     console.log('Ready and connected to Discord.')
+// });
+
+// client.on('messageCreate', async (message) => {
+//     if (!message.content.startsWith(config.commandPrefix) || message.author.bot) {
+//         return; //Ignore messages that don't start with the command prefix or are sent by bots.
+//     };
+
+//     const [command, ...commandArguments] = message.content.slice(config.commandPrefix.length).trim().split(/ +/g).map(arg => arg.toLowerCase());
+
+//     switch (command) {
+//         case `${commandName}`:
+//             if (!message.member.roles.cache.some(role => config.allowedRoles.includes(role.name))) {
+//                 return; //Ignore commands sent by users without allowed roles.
+//             }
+//             await generateCSV(message);
+//             console.log(`Generating CSV file for ${message.guild.name} server. "${message.member.user.username} requested this file."`);
+//             message.delete({ timeout: 250 });
+//             break;
+//         default:
+//             return; //Ignore any other commands.
+//     }
+// });
+
+// client.login(process.env.BOT_TOKEN);
--- src/functions/csvGenerator.js
+++ src/functions/csvGenerator.js
@@ -1,42 +1,3 @@
-// const fs = require('fs');
-// const { Parser } = require('json2csv');
-
-// const generateCSV = async (message) => {
-//     console.log(message);
-//     const guild = await message.guild.fetch();
-//     const members = await guild.members.fetch();
-//     const allMembers = members
-//         .filter(member => !member.user.bot)
-//         .map(member => ({
-//             Name: member.user.displayName,
-//             Tag: member.user.tag,
-//             UserID: `User ID: ${member.user.id}`,
-//             'Joined Date': member.joinedAt.toDateString(),
-//             Roles: member.roles.cache.map(role => role.name).join(', ')
-//         }));
-
-//     const json2csvParser = new Parser();
-//     const csv = json2csvParser.parse(allMembers);
-//     const filePath = `./csv_files/${guild.name}_members.csv`;
-
-//     fs.writeFileSync(filePath, csv, { flag: "a" }, (error) => {
-//         if (error) {
-//             console.log(`Error writing file: ${error.message}`)
-//         }
-//     });
-
-//     const stream = fs.createReadStream(filePath);
-//     message.channel.send({
-//         files: [{
-//             attachment: stream,
-//             name: `${guild.name}_members.csv`
-//         }]
-//     });
-// }
-
-// module.exports = { generateCSV };
-
-
 const fs = require('fs');
 const { Parser } = require('json2csv');
 
@@ -86,3 +47,41 @@ const generateCSV = async (message, requestedRoles) => {
 }
 
 module.exports = { generateCSV };
+
+// const fs = require('fs');
+// const { Parser } = require('json2csv');
+
+// const generateCSV = async (message) => {
+//     console.log(message);
+//     const guild = await message.guild.fetch();
+//     const members = await guild.members.fetch();
+//     const allMembers = members
+//         .filter(member => !member.user.bot)
+//         .map(member => ({
+//             Name: member.user.displayName,
+//             Tag: member.user.tag,
+//             UserID: `User ID: ${member.user.id}`,
+//             'Joined Date': member.joinedAt.toDateString(),
+//             Roles: member.roles.cache.map(role => role.name).join(', ')
+//         }));
+
+//     const json2csvParser = new Parser();
+//     const csv = json2csvParser.parse(allMembers);
+//     const filePath = `./csv_files/${guild.name}_members.csv`;
+
+//     fs.writeFileSync(filePath, csv, { flag: "a" }, (error) => {
+//         if (error) {
+//             console.log(`Error writing file: ${error.message}`)
+//         }
+//     });
+
+//     const stream = fs.createReadStream(filePath);
+//     message.channel.send({
+//         files: [{
+//             attachment: stream,
+//             name: `${guild.name}_members.csv`
+//         }]
+//     });
+// }
+
+// module.exports = { generateCSV };
